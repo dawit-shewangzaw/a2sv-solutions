c799807af78a4754f28c4214709ae16e3ef1ada7
Dave <121303027+dawit-shewangzaw@users.noreply.github.com>
2026-02-26 09:54:22 +0300

    Add solution for Insertion Sort (trial 1)

---
+++ hackerrank/insertion-sort_trial1.py
@@ -0,0 +1,11 @@
+def insertionSort1(n, arr):
+    key = arr[-1]
+    i = n - 2
+
+    while i >= 0 and arr[i] > key:
+        arr[i + 1] = arr[i]
+        print(*arr)
+        i -= 1
+
+    arr[i + 1] = key
+    print(*arr)
